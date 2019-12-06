bf05cf722988e94de3d9ad241379575f90bfb1e2
taranov <mr.alexey.taranov@yandex.ru>
2019-12-06 23:20:40 +0300

    Add simple Security support

---
+++ src/main/java/ru/taranov/domain/Role.java
@@ -0,0 +1,5 @@
+package ru.taranov.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/ru/taranov/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package ru.taranov.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.taranov.domain.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
