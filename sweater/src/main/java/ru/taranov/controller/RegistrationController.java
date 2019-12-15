301211b9f6abd6a64652b235dcb346e9435d1d6c
taranov <mr.alexey.taranov@yandex.ru>
2019-12-15 21:20:06 +0300

    Password encoding. Bean validation.

--- sweater/src/main/java/ru/taranov/controller/RegistrationController.java
+++ sweater/src/main/java/ru/taranov/controller/RegistrationController.java
@@ -3,12 +3,14 @@ package ru.taranov.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.taranov.domain.User;
 import ru.taranov.service.UserService;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -29,9 +31,19 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
+    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
+        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
+            model.addAttribute("passwordError", "Passwords are different!");
+        }
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errors);
+            return "registration";
+        }
+
         if (!userService.addUser(user)) {
-            model.put("message", "User exists!");
+            model.addAttribute("usernameError", "User exists!");
             return "registration";
         }
 
