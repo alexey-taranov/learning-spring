f91ef283564366377a7cbecd0bbd73e2249d4c88
taranov <mr.alexey.taranov@yandex.ru>
2019-12-18 13:42:04 +0300

    Add captcha check. Spring session and rememberMe.

--- sweater/src/main/java/ru/taranov/domain/User.java
+++ sweater/src/main/java/ru/taranov/domain/User.java
@@ -19,9 +19,7 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "password cannot be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "password confirmation cannot be empty")
-    private String password2;
+
     private boolean active;
 
     @Email(message = "Email is not correct")
@@ -88,14 +86,6 @@ public class User implements UserDetails {
 
     public void setActivationCode(String activationCode) {
         this.activationCode = activationCode;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 
     @Override
