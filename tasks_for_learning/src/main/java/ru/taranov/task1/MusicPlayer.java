b7de9a935214b7ab5551bd341ac34e5fcb53ab9c
taranov <mr.alexey.taranov@yandex.ru>
2019-12-23 13:14:46 +0300

    update task.

--- tasks_for_learning/src/main/java/ru/taranov/task1/MusicPlayer.java
+++ tasks_for_learning/src/main/java/ru/taranov/task1/MusicPlayer.java
@@ -1,8 +1,11 @@
 package ru.taranov.task1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
 
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
@@ -24,17 +27,19 @@ public class MusicPlayer {
     }
 
     // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public MusicPlayer() {}
 
-    public void setMusic(Music music) {
-        this.music = music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    void playMusic() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
---
+++ tasks_for_learning/src/main/java/ru/taranov/task1/RapMusic.java
@@ -0,0 +1,8 @@
+package ru.taranov.task1;
+
+public class RapMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Get low";
+    }
+}
